37a216910e26eb21ae08d4d9ce23b0312424af15
yuyatanno <11t5045c@shinshu-u.ac.jp>
2015-04-27 22:14:22 +0900

    first commit

---
+++ kadai/ex2_4.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 23 01:37:38 2015
+
+@author: 11T5045C
+"""
+#1.
+pi=3.14
+print (4.0/3.0)*pi*(5.0**3.0)
+
+#2.
+i=24.95*0.4
+book=24.95-9.98
+book_1=book+3
+
+book_2=book+0.75
+sum = book_2 * 59
+print book_1 + sum
+
+#3.
+s_walk=8.15
+m_walk=7.12
+x=m_walk*3
+y=s_walk*2
+
+z=x+y
+
+print 'am7:30'
